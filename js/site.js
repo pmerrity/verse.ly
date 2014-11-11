31f056c4f4cf0151974bfb2144790ae03a7a7a86
pmerrity <pmerrity@sister.tv>
2014-11-11 16:50:59 -0600

    #finally

--- js/site.js
+++ js/site.js
@@ -1,34 +1,59 @@
 $.ajax({
-	type : 'GET',
-	url : 'https://ckwO6bJ9byAuO1aYnsR303owo795tbMK9LGGMjpU:bibles.org/v2/verses/eng-esv:Acts.8.34.js',
-	crossdomain : 'true',
-//	data: {"ckwO6bJ9byAuO1aYnsR303owo795tbMK9LGGMjpU":"ckwO6bJ9byAuO1aYnsR303owo795tbMK9LGGMjpU"},
-	jsonp: "false",
-	dataType : 'jsonp',
-//	contentType: "application/x-www-form-urlencoded;charset=ISO-8859-15",
-	success : function(response){
-		console.log(response);
-		console.log("success");
+	url:'http://getbible.net/json',
+	dataType: 'jsonp',
+	data: 'p=John1&v=kjv',
+	jsonp: 'getbible',
+	success:function(json){
+		// set text direction
+		if (json.direction == 'RTL'){
+			var direction = 'rtl';
+		} else {
+			var direction = 'ltr';
+		}
+		// check response type
+		if (json.type == 'verse'){
+			var output = '';
+			$.each(json.book, function(index, value) {
+				output += '<div><b>'+value.book_name+' '+value.chapter_nr+'</b></div><br/><p class="'+direction+'">';
+				$.each(value.chapter, function(index, value) {
+					output += '  <span class="ltr">' +value.verse_nr+ '</span>  ';
+					output += value.verse;
+					output += '<br/>';
+				});
+				output += '</p>';
+			});
+			$('#scripture').html(output);  // <---- this is the div id we update
+		}
+
+		else if (json.type == 'chapter'){
+			var output = '<div id="chapter"><b>'+json.book_name+' '+json.chapter_nr+'</b></div><br/><p class="'+direction+'">';
+			$.each(json.chapter, function(index, value) {
+				output += '  <span class="ltr">' +value.verse_nr+ '</span>  ';
+				output += value.verse;
+				output += '<br/>';
+			});
+			output += '</p>';
+			$('#scripture').html(output);  // <---- this is the div id we update
+		}
+
+		else if (json.type == 'book'){
+			var output = '';
+			$.each(json.book, function(index, value) {
+				output += '<div id="book"><b>'+json.book_name+' '+value.chapter_nr+'</b></div><br/><p class="'+direction+'">';
+				$.each(value.chapter, function(index, value) {
+					output += '  <span class="ltr">' +value.verse_nr+ '</span>  ';
+					output += value.verse;
+					output += '<br/>';
+				});
+				output += '</p>';
+			});
+			if(addTo){
+				$('#scripture').html(output);  // <---- this is the div id we update
+			}
+		}
+
 	},
-	error : function(response){
-		console.log(JSON.stringify(response));
-		console.log("fail");
-	}
+	error:function(){
+		$('#scripture').html('<h2>No scripture was returned, please try again!</h2>'); // <---- this is the div id we update
+	},
 });
-
-//$.getJSON('https://ckwO6bJ9byAuO1aYnsR303owo795tbMK9LGGMjpU:bibles.org/v2/verses/eng-esv:Acts.8.34.js', function(k,v) {
-//	console.log(k,v)
-//})
-
-//ckwO6bJ9byAuO1aYnsR303owo795tbMK9LGGMjpU
-
-
-//function callApi(api){
-//	var s = document.createElement('script');
-//	s.setAttribute('src',api);
-//	document.getElementsByTagName('head')[0].appendChild(s);
-//}
-//var api = 'YOUR HTTP GET URL';
-//callApi(api);
-
-//ckwO6bJ9byAuO1aYnsR303owo795tbMK9LGGMjpU
